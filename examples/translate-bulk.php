9d87c0de69f293e09662843e2a135ff495335210
Gino Pane <sergey.karavay@gmail.com>
2017-12-19 22:52:32 +0300

    Updates: - added example for bulk translation

---
+++ examples/translate-bulk.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use GinoPane\PHPolyglot\PHPolyglot;
+
+try {
+    $phpolyglot = new PHPolyglot();
+
+    $textToTranslate = ['Hi!', 'I am PHPolyglot - an easy-to-use library for translation', 'Happy coding!'];
+
+    $languages = ['it', 'de', 'es', 'ru', 'fi', 'be', 'en'];
+    $languageFrom = 'en';
+
+    foreach($languages as $languageTo) {
+        $response = $phpolyglot->translateBulk($textToTranslate, $languageTo, $languageFrom);
+
+        echo $languageTo . PHP_EOL;
+        echo $response . PHP_EOL;
+        echo PHP_EOL;
+    }
+} catch (Exception $exception) {
+    $errorMessage = $exception->getMessage();
+
+    echo sprintf("Error happened: %s", $errorMessage);
+}
+
+echo PHP_EOL;
