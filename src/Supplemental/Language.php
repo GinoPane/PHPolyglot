31f6401fecda6eb442666a3f122719e8d4d25a30
Sergey Karavay <siarheikaravai@rentalcars.com>
2017-12-21 12:43:01 +0000

    Updates: - added Language class and interface; constants for language codes.

---
+++ src/Supplemental/Language.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GinoPane\PHPolyglot\Supplemental;
+
+use ReflectionClass;
+
+/**
+ * Class Language
+ *
+ * @author: Sergey <Gino Pane> Karavay
+ */
+class Language implements LanguageInterface
+{
+    private static $constants = [];
+
+    /**
+     * Checks if code is valid
+     *
+     * @param string $code
+     *
+     * @return bool
+     */
+    public function codeIsValid(string $code): bool
+    {
+        return array_search($code, $this->getConstants()) !== false;
+    }
+
+    /**
+     * Fills constants array if it is empty and returns it
+     *
+     * @return array
+     */
+    private function getConstants(): array
+    {
+        if (empty(self::$constants)) {
+            self::$constants = (new ReflectionClass($this))->getConstants();
+        }
+
+        return self::$constants;
+    }
+
+}
