cf27eb1cfc64987c2df8ca4ceb29e86e7e185db0
Gino Pane <sergey.karavay@gmail.com>
2017-12-31 17:04:12 +0300

    Happy New 2018 Year: - some work on dictionary API.

---
+++ src/API/Implementation/Dictionary/DictionaryApiAbstract.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GinoPane\PHPolyglot\API\Implementation\Dictionary;
+
+use GinoPane\PHPolyglot\API\Response\Dictionary\DictionaryResponse;
+
+/**
+ * Interface DictionaryApiInterface
+ *
+ * @author Sergey <Gino Pane> Karavay
+ */
+abstract class DictionaryApiAbstract implements DictionaryApiInterface
+{
+    /**
+     * Gets text alternatives either in the same language (possible text forms)
+     * or in different language (alternative translations)
+     *
+     * @param string $text
+     * @param string $languageFrom
+     * @param string $languageTo
+     *
+     * @return DictionaryResponse
+     */
+    public function getTextAlternatives(
+        string $text,
+        string $languageFrom,
+        string $languageTo = ''
+    ): DictionaryResponse {
+
+    }
+}
--- src/API/Implementation/Dictionary/Yandex/YandexDictionaryApi.php
+++ src/API/Implementation/Dictionary/Yandex/YandexDictionaryApi.php
@@ -2,14 +2,38 @@
 
 namespace GinoPane\PHPolyglot\API\Implementation\Dictionary\Yandex;
 
-use GinoPane\PHPolyglot\API\Implementation\Dictionary\DictionaryApiInterface;
+use GinoPane\PHPolyglot\API\Supplemental\Yandex\YandexApiErrorsTrait;
+use GinoPane\PHPolyglot\API\Implementation\Dictionary\DictionaryApiAbstract;
 
 /**
  * Class YandexDictionaryApi
  *
  * @author Sergey <Gino Pane> Karavay
  */
-class YandexDictionaryApi implements DictionaryApiInterface
+class YandexDictionaryApi extends DictionaryApiAbstract
 {
+    /**
+     * Main API endpoint
+     *
+     * @var string
+     */
+    protected $apiEndpoint = 'https://translate.yandex.net/api/v1.5/tr.json';
 
+    /**
+     * API key required for calls
+     *
+     * @var string
+     */
+    protected $apiKey = '';
+
+    /**
+     * Mapping of properties to environment variables which must supply these properties
+     *
+     * @var array
+     */
+    protected $envProperties = [
+        'apiKey' => 'YANDEX_TRANSLATE_API_KEY'
+    ];
+
+    use YandexApiErrorsTrait;
 }
