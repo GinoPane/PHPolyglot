d5fcedb11306c56e1c9dfd06f54ebc73808c573b
Gino Pane <sergey.karavay@gmail.com>
2018-01-11 22:36:17 +0300

    Updates: - format change.

--- src/API/Supplemental/TTS/TtsAudioFormats.php
+++ src/API/Supplemental/TTS/TtsAudioFormats.php
@@ -13,17 +13,17 @@ namespace GinoPane\PHPolyglot\API\Supplemental\TTS;
  */
 interface TtsAudioFormats
 {
-    const AUDIO_BASIC = "basic";
-    const AUDIO_FLAC = "flac";
-    const AUDIO_L16 = "l16";
-    const AUDIO_MP3 = "mp3";
-    const AUDIO_MPEG = "mpeg";
-    const AUDIO_MULAW = "mulaw";
-    const AUDIO_OGG = "ogg";
-    const AUDIO_OGG_OPUS = "ogg_opus";
-    const AUDIO_OGG_VORBIS = "ogg_vorbis";
-    const AUDIO_WAV = "wav";
-    const AUDIO_WEBM = "webm";
-    const AUDIO_WEBM_OPUS = "webm_opus";
+    const AUDIO_BASIC       = "basic";
+    const AUDIO_FLAC        = "flac";
+    const AUDIO_L16         = "l16";
+    const AUDIO_MP3         = "mp3";
+    const AUDIO_MPEG        = "mpeg";
+    const AUDIO_MULAW       = "mulaw";
+    const AUDIO_OGG         = "ogg";
+    const AUDIO_OGG_OPUS    = "ogg_opus";
+    const AUDIO_OGG_VORBIS  = "ogg_vorbis";
+    const AUDIO_WAV         = "wav";
+    const AUDIO_WEBM        = "webm";
+    const AUDIO_WEBM_OPUS   = "webm_opus";
     const AUDIO_WEBM_VORBIS = "webm_vorbis";
 }
